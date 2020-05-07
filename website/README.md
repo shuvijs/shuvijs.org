a2ef619042b5267edf3c27c84a364c2082a1a764
Heyong Nie <niehey@163.com>
2020-05-07 18:19:08 +0800

    init

---
+++ website/README.md
@@ -0,0 +1,25 @@
+# Website
+
+This website is built using Docusaurus 2, a modern static website generator.
+
+## Installation
+
+```shell
+yarn
+```
+
+## Local Development
+
+```shell
+yarn start
+```
+
+This command starts a local development server and open up a browser window. Most changes are reflected live without having to restart the server.
+
+## Build
+
+```shell
+yarn build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
---
+++ website/docusaurus.config.js
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const siteConfig = {
+  title: 'Shuvijs',
+  tagline: 'Shuvijs',
+  url: 'https://shuvijs.github.io/shuvijs.org/',
+  baseUrl: '/shuvijs.org/',
+  projectName: 'shuvijs-doc',
+  organizationName: 'Shuvijs',
+  favicon: 'img/favicon/favicon.ico',
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          path: '../docs',
+          sidebarPath: require.resolve('./sidebars.json'),
+          editUrl: 'https://github.com/shuvijs/shuvijs.org',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  themeConfig: {
+    image: 'img/logo-og.png',
+    // algolia: {
+    //   apiKey: '',
+    //   indexName: '',
+    // },
+    navbar: {
+      title: 'Shuvijs',
+      logo: {
+        alt: 'Shuvijs',
+        src: 'img/logo.svg',
+      },
+      links: [
+        { to: 'docs/getting-started', label: 'Docs', position: 'right' },
+        {
+          href: 'https://github.com/shuvijs/shuvi/issues',
+          label: 'Help',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/shuvijs/shuvi',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Get Started',
+              to: 'docs/getting-started',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/shuvijs/shuvi',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Shuvijs`,
+    },
+  },
+}
+
+module.exports = siteConfig
